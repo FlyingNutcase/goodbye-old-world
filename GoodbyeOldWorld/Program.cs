abac691263124766e251e574d9530d298f56e270
FlyingNutcase <flying.nutcase@gmail.com>
2025-02-08 21:51:01 +1300

    Add message indicating upcoming feature addition in Program.cs and doing a Commit AND Push

--- GoodbyeOldWorld/Program.cs
+++ GoodbyeOldWorld/Program.cs
@@ -8,3 +8,4 @@ for (int i = 0; i < fruit.Length; i++)
 }
 
 Console.WriteLine("Hey, I'm now on branch alt1. How about that?");
+Console.WriteLine("I'm going to add a new feature to this branch and I will commmit and push it to the remote repository.");
