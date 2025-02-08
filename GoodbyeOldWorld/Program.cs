e29d3c94f50a9a86eb228be6d0552a31338b09cd
FlyingNutcase <flying.nutcase@gmail.com>
2025-02-08 19:11:19 +1300

    Array cycling

---
+++ GoodbyeOldWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Goodbye, Old World!");
+string[] fruit = { "apple", "orange", "pear", "banana" };
+
+for (int i = 0; i < fruit.Length; i++)
+{
+    Console.WriteLine(fruit[i]);
+}
